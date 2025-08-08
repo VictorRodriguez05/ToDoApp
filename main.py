69beead5df4d86f979d0d10b2f489518c5184809
VictorRodriguez05 <victordanilorodriguezruiz@hotmail.com>
2025-08-07 19:00:30 -0500

    Inicializar proyecto ToDoApp

---
+++ main.py
@@ -0,0 +1,10 @@
+from task_model import TaskModel
+
+def main():
+    tas = TaskModel("estudiar para el examen")
+    print(f"tarea creada: {task.get_task_name()}")
+
+
+
+if __name__ == "__name__":
+   main()   
