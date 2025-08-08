ba61d72bbadbdd24fdaddaf5e0915ed76681d784
VictorRodriguez05 <victordanilorodriguezruiz@hotmail.com>
2025-08-07 19:33:19 -0500

    Cambios en main.py desde group-1

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 from task_model import TaskModel
 
 def main():
-    task = TaskModel("estudiar para el examen")
-    print(f"tarea creada: {task.get_task_name()}")
-    task.mark_as_complete()
-    print(f"Tarea completada:  {task.is_completdC}")
-    task.remove_task()
-    print(f"tarea eliminada:  {task.get_task_name()}")
+    task = TaskModel("hacer tarea")
+    print(f"Tarea: {task.get_task_name()}")
+    task.set_done()
+    print("Tarea marcada como hecha")
 
-
-
-if __name__ == "__name__":
-   main()   
+if __name__ == "__main__":
+    main()
